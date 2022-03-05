68f0159bf35c298606321f3b8c37a4a96d6cdc66
kevniu95 <kniu@uchicago.edu>
2022-03-05 02:16:33 -0600

    Begin moving toward non-pinchable scroll view implementation

--- bracketSimulator/bracketSimulator/View Controllers/NewEntryViewController.swift
+++ bracketSimulator/bracketSimulator/View Controllers/NewEntryViewController.swift
@@ -10,82 +10,67 @@ import UIKit
 class NewEntryViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var teamCell: UIStackView!
     @IBOutlet weak var scrollView: UIScrollView!
-    var teamCells: [UIStackView] = []
-    
-    
-    let bracketImageView = UIImageView(image: UIImage(named: "bracket2022"))
-    
-    
+    let bracketFrameScaler = 3.0
+    var teamCells = [UIStackView]()
+        
     override func viewDidLoad() {
         // Do any additional setup after loading the view.
         super.viewDidLoad()
         initiateScrollView()
-//        scrollView.addSubview(teamCell)
-//        initiateTeamCells()
-    }
-    
-    override func viewDidAppear(_ animated: Bool){
-        super.viewDidAppear(animated)
-        scrollView.setZoomScale(0.1, animated: true)
+        initiateTeamCells()
     }
     
     func initiateScrollView(){
-        addBracketImage()
         scrollView.delegate = self
-        scrollView.maximumZoomScale = 0.6
-        scrollView.setZoomScale(0.5, animated: true)
-        scrollView.contentOffset = CGPoint(x:scrollView.contentSize.width * 0.45 ,
+        guard let bracketImageView = initializeBracketImage() else{
+            return
+        }
+        addBracketImage(bracketImageView: bracketImageView)
+        
+//        scrollView.maximumZoomScale = 0.6
+//        scrollView.setZoomScale(0.5, animated: true)
+        scrollView.contentSize = CGSize(width: bracketImageView.frame.size.width, height: bracketImageView.frame.size.height)
+        scrollView.contentOffset = CGPoint(x:scrollView.contentSize.width * 0.39 ,
                                            y:scrollView.contentSize.height * 0.5)
+        
     }
-//    
-//    func initiateTeamCells(){
-//        for _ in 1...5{
-//            teamCells.append(teamCell)
-//        }
-//        for cell in teamCells{
-//            scrollView.addSubview(cell)
+    
+    func initializeBracketImage()-> UIImageView?{
+        let bracketImageView: UIImageView = UIImageView()
+        guard let bracketImage = UIImage(named: "bracket2022") else {
+            print("Couldn't create UI Image!")
+            return nil
+        }
+        bracketImageView.frame.size.width = bracketImage.size.width/bracketFrameScaler
+        bracketImageView.frame.size.height = bracketImage.size.height/bracketFrameScaler
+        bracketImageView.image =  UIImage(named: "bracket2022")
+        return bracketImageView
+    }
+    
+    func initiateTeamCells(){
+        for _ in 1...1{
+            teamCells.append(teamCell)
+        }
+        for cell in teamCells{
+            scrollView.addSubview(cell)
+            print("Added cell")
 //            fitToConstraint(teamCell: cell)
-//        }
-//        
-//    }
-//    
-//    
+        }
+
+    }
+    
+//
 //    func fitToConstraint(teamCell: UIStackView){
-//        let leadingConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: scrollView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1, constant: 0)
-//        let trailingConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: scrollView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1, constant: 0)
-//        let topConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: scrollView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0)
+//        let leadingConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: bracketImageView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1, constant: 0)
+//        let trailingConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: bracketImageView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1, constant: 0)
+//        let topConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: bracketImageView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 150)
 //        let bottomConstraint = NSLayoutConstraint(item: teamCell, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: scrollView, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
-//        NSLayoutConstraint.activate([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
-//    }
-    
-    
-    
-    
-//    override func viewWillLayoutSubviews() {
-//        super.viewWillLayoutSubviews()
-//        for index, imageView in
+//        NSLayoutConstraint.activate([leadingConstraint, topConstraint
+//                                    ])
 //    }
 //
-    func addBracketImage(){
+
+    func addBracketImage(bracketImageView: UIImageView){
         scrollView.addSubview(bracketImageView)
-//        print(bracketImageView)
-//        bracketImageView.translatesAutoresizingMaskIntoConstraints = false
-
-
     }
-    
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-      return bracketImageView
-    }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
