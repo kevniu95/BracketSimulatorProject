1106b1aba5d0057958bb8e8c45ebcb3e1218c835
kevniu95 <kniu@uchicago.edu>
2022-03-04 22:25:56 -0600

    Establish initial View Controllers

---
+++ bracketSimulator/bracketSimulator/View Controllers/NewEntryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewEntryViewController.swift
+//  bracketSimulator
+//
+//  Created by Kevin Niu on 3/4/22.
+//
+
+import UIKit
+
+class NewEntryViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
